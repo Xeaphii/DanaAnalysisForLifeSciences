8cbc404f1a2cf626711ec811621414716c4a5dd2
xeaphii <muhammadzeeshan020@gmail.com>
2016-08-25 10:49:50 -0700

    Reading and operting with csv's

---
+++ Statistics and R/Week 1/Downloader.R
@@ -0,0 +1,4 @@
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
+filename <- "femaleMiceWeights.csv" 
+download(url, destfile=filename)
---
+++ Statistics and R/Week 1/ReadingFromCSV.R
@@ -0,0 +1,8 @@
+getwd()
+
+setwd("E:/Learn/Data Analysis for LIfe Sciences/DanaAnalysisForLifeSciences/Statistics and R/Week 1")
+
+getwd()
+
+dat<-read.csv("femaleMiceWeights.csv")
+
