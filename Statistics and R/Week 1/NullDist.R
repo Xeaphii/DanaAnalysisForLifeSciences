413fa38e3e28a348a3b7f9df83436bb4e9b26dd5
xeaphii <muhammadzeeshan020@gmail.com>
2016-08-25 12:52:05 -0700

    Hist of various sample sizes explored

--- Statistics and R/Week 1/NullDist.R
+++ Statistics and R/Week 1/NullDist.R
@@ -3,14 +3,20 @@ population <- read.csv("femaleControlsPopulation.csv")
 population <- unlist(population) 
 
 set.seed(1)
-n <- 10000
+n <- 1000
 null <- vector("numeric",n)
 for (i in 1:n) {
-  treatment <- sample(population,5)
+  treatment <- sample(population,50)
   null[i] <- mean(treatment)
 }
+
+mean(null<25) - mean(null<23)
+
 hist(mean(population)-null)
 
 
 mean(abs((mean(population)-null)) > 1)
 mean(abs(null) > 1)
+
+pnorm(25, mean = 23.9 , sd = 0.43, lower.tail = TRUE, log.p = FALSE) -
+  pnorm(23, mean = 23.9 , sd = 0.43, lower.tail = TRUE, log.p = FALSE)
