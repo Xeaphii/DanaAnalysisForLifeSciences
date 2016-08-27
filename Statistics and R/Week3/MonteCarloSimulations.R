1bc985f4a91da076a2b2c255f7661f8625725c00
XEAPHII <muhammadzeeshan020@gmail.com>
2016-08-27 15:28:07 +0500

    Befre starting week 4.

---
+++ Statistics and R/Week3/MonteCarloSimulations.R
@@ -0,0 +1,25 @@
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/babies.txt"
+filename <- basename(url)
+download(url, destfile=filename)
+babies <- read.table("babies.txt", header=TRUE)
+bwt.nonsmoke <- filter(babies, smoke==0) %>% select(bwt) %>% unlist 
+bwt.smoke <- filter(babies, smoke==1) %>% select(bwt) %>% unlist
+
+
+N=10
+set.seed(1)
+nonsmokers <- sample(bwt.nonsmoke , N)
+smokers <- sample(bwt.smoke , N)
+obs <- median(smokers) - median(nonsmokers)
+
+
+dat <- c(smokers,nonsmokers)
+set.seed(1)
+avgdiff <- replicate(1000, {
+  shuffle <- sample( dat )
+  smokersstar <- shuffle[1:N]
+  nonsmokersstar <- shuffle[(N+1):(2*N)]
+  median(smokersstar)-median(nonsmokersstar)
+})
+
+(sum(abs(avgdiff) > abs(obs)) + 1) / (length(avgdiff) + 1)
