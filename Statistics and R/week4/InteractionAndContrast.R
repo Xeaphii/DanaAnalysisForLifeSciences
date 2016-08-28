2ad66e6ade09262624a4365dea009873bb354ce3
XEAPHII <muhammadzeeshan020@gmail.com>
2016-08-28 09:57:31 +0500

    Before starting High Thrughput course

--- Statistics and R/week4/InteractionAndContrast.R
+++ Statistics and R/week4/InteractionAndContrast.R
@@ -45,3 +45,14 @@ summary(fitX)
 anova(fitX)
 L3vsL2 <- contrast(fitX,list(leg="L2",type="push"),list(leg="L1",type="push"))
 L3vsL2
+
+
+fit <- lm(friction ~ type + leg, data=spider)
+betahat <- coef(fit)
+Y <- matrix(spider$friction, ncol=1)
+X <- model.matrix(~ type + leg, data=spider)
+
+QR <- qr(X)
+Q <- qr.Q( QR )
+R <- qr.R( QR )
+(betahat <- backsolve(R, crossprod(Q,Y) ) )
