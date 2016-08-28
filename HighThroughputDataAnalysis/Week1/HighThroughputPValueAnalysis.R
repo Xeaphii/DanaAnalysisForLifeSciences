54825db715ac83c5cbcbb7fe4110f61ef31aaa51
XEAPHII <muhammadzeeshan020@gmail.com>
2016-08-28 14:08:45 +0500

    Before break

--- HighThroughputDataAnalysis/Week1/HighThroughputPValueAnalysis.R
+++ HighThroughputDataAnalysis/Week1/HighThroughputPValueAnalysis.R
@@ -19,8 +19,8 @@ cases = rnorm(10,30,2)
 controls = rnorm(10,30,2)
 t.test(cases,controls)$p.val
 })
-
 sum(avgs<=0.05)
+
 set.seed(100)
 pvals <- replicate(20,{
   cases = rnorm(10,30,2)
@@ -31,12 +31,25 @@ sum(pvals<=0.05)
 
 
 set.seed(100)
-avgs<- replicate(1000,{
+avgs<- replicate(8793,{
   pvals <- replicate(20,{
     cases = rnorm(10,30,2)
     controls = rnorm(10,30,2)
     t.test(cases,controls)$p.val
   })
-  sum(pvals<=0.05)
+  return(sum(pvals<=0.05))
 })
 mean(avgs>0)
+
+set.seed(100)
+pvals <- replicate(8793,{
+  cases = rnorm(10,30,2)
+  controls = rnorm(10,30,2)
+  t.test(cases,controls)$p.val
+})
+mean(pvals<=0.05)
+
+B<-1000
+minpval <- replicate(B, min(runif(8793,0,1))<0.000007)
+mean(minpval>=1)
+
